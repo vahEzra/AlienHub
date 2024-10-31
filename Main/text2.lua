b676e048f636393e903d1e51f493ce20664db52d
vahEzra <lifecycle702@gmail.com>
2024-10-31 14:20:18 -0400

    Create text2.lua

---
+++ Main/text2.lua
@@ -0,0 +1,27 @@
+if game.Players.LocalPlayer:WaitForChild("PlayerGui").display_text then
+-- nope!
+else
+local display_text = Instance.new("ScreenGui")
+local text = Instance.new("TextBox")
+display_text.Name = "display_text"
+display_text.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+display_text.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+display_text.ResetOnSpawn = false
+text.Name = "text"
+text.Parent = display_text
+text.BackgroundColor3 = Color3.fromRGB(13, 13, 13)
+text.BorderColor3 = Color3.fromRGB(0, 0, 0)
+text.BorderSizePixel = 0
+text.Position = UDim2.new(0.14965035, 0, 0.904884338, 0)
+text.Size = UDim2.new(0, 1001, 0, 20)
+text.Font = Enum.Font.Unknown
+text.PlaceholderColor3 = Color3.fromRGB(178, 178, 178)
+text.PlaceholderText = "# entertext here"
+text.Text = ""
+text.TextColor3 = Color3.fromRGB(178, 178, 178)
+text.TextSize = 14.000
+text.TextXAlignment = Enum.TextXAlignment.Left
+text:GetPropertyChangedSignal("Text"):Connect(function()
+	game.TextChatService:DisplayBubble(game.Players.LocalPlayer.Character.Head, text.Text)
+end)
+end
