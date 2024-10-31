496395a824025411c0226777dc2abffa24c84c1f
vahEzra <lifecycle702@gmail.com>
2024-10-31 13:44:13 -0400

    Update Loader.lua

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -1,3 +1,5 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "alienHub Loading Menu",
    LoadingTitle = "alienHub Free Version",
